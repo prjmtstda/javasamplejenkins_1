6e3cf525f0d0685f129de4329a2ea4ee8dd26a56
QCT <snkta2018@gmail.com>
2023-06-03 08:54:30 +0530

    Commit

--- src/pkg/Manager_1.java
+++ src/pkg/Manager_1.java
@@ -13,6 +13,7 @@ public class Manager_1
 		System.out.println("Done4");
 		System.out.println("Done5");
 		System.out.println("Done6");
-		System.out.println("Done");
+		System.out.println("Done7");
+		
 	}
 }
